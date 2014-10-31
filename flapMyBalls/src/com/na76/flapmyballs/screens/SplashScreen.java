c0e042095d89cd53f74cea201cff97af9b9a86bb
Rheriel <rheriel@hotmail.co.jp>
2014-10-31 00:47:33 -0600

    WIP HM Splash Image

--- flapMyBalls/src/com/na76/flapmyballs/screens/SplashScreen.java
+++ flapMyBalls/src/com/na76/flapmyballs/screens/SplashScreen.java
@@ -3,12 +3,8 @@ package com.na76.flapmyballs.screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -16,27 +12,19 @@ import com.na76.flapmyballs.constants.GameConstants;
 
 public class SplashScreen implements Screen {
 
-	private Texture splash = new Texture(Gdx.files.internal("HM-Splash.png"));
-	private Image splashImage;
+	private Texture splash = new Texture(Gdx.files.internal("na76_logo.png"));
+	private Image splashImage = new Image(splash);
 	private Stage stage = new Stage();
-	private ShapeRenderer shapeRenderer= new ShapeRenderer();
 
 	public SplashScreen(){
 		Gdx.app.log("Splash Screen", "Constructor called");
-		splash.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		splashImage = new Image(splash);
 	}
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0,0,0,0); //sets clear color to white
+		Gdx.gl.glClearColor(0,0,0,1); //sets clear color to black
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //clear the batch
-        shapeRenderer.begin(ShapeType.Filled); 
-        shapeRenderer.setColor(Color.WHITE); 
-        shapeRenderer.rect(0, 0, GameConstants.GAME_WIDTH * 2, GameConstants.GAME_HEIGHT * 2);
-        shapeRenderer.end();
-        splashImage.setScale(.6f);
-        splashImage.setX((GameConstants.GAME_WIDTH / 2) - (splashImage.getImageWidth()) / 6); // TODO Place in the middle
+        splashImage.setX((GameConstants.GAME_WIDTH) - (splashImage.getImageWidth()/2));
         splashImage.setY((GameConstants.GAME_HEIGHT) - (splashImage.getImageHeight()/2));
         stage.act();
         stage.draw();
@@ -80,7 +68,6 @@ public class SplashScreen implements Screen {
 	public void dispose() {
 		splash.dispose();
         stage.dispose();
-        shapeRenderer.dispose();
 	}
 
 }
